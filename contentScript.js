eb013b985d69a1243616eefeb444705f6a2f161b
Benjamin <benctw@gmail.com>
2024-09-06 11:39:07 +0800

    Regex

--- contentScript.js
+++ contentScript.js
@@ -5,20 +5,20 @@ pageTitles.forEach(function (pageTitle) {
     var content = pageTitle.textContent; // 取出內容
 
     // 正則表達式來匹配所需內容
-    // var regex = /[A-Za-z]{2}\d{4}/;
-    // var match = content.match(regex);
-    // var result = match ? match[0] : null; // 如果匹配成功，則取匹配的結果
+    var regex = /\w{2}\d{4}[\*A-Za-z]|\d{8}/;
+    var match = content.match(regex);
+    var result = match ? match[0] : null; // 如果匹配成功，則取匹配的結果
 
     // 創建超連結元素
     var link = document.createElement('a');
-    link.textContent = ' myGPTs';
+    link.textContent = ' 中央機房';
     var hostname = window.location.hostname;
     if (hostname === "ncueeclass.ncu.edu.tw") {
-        link.href = 'https://nc.uedu.tw/goto/' + content;
+        link.href = 'https://nc.uedu.tw/goto/' + result;
     } else if (hostname === "im.tmu.edu.tw") {
-        link.href = 'https://tm.uedu.tw/goto/' + content;
+        link.href = 'https://tm.uedu.tw/goto/' + result;
     } else {
-        link.href = 'https://uedu.tw/goto/' + content; // 預設連結
+        link.href = 'https://uedu.tw/goto/' + result; // 預設連結
     }
     // link.href = 'https://ncuedu.tw/browser/extension/goto/mygpts/' + content;
 
@@ -42,19 +42,19 @@ pageTitles.forEach(function (pageTitle) {
     var content = pageTitle.textContent; // 取出內容
 
     // 正則表達式來匹配所需內容
-    // var regex = /[A-Za-z]{2}\d{4}/;
-    // var match = content.match(regex);
-    // var result = match ? match[0] : null; // 如果匹配成功，則取匹配的結果
+    var regex = /\w{2}\d{4}[\*A-Za-z]|\d{8}/;
+    var match = content.match(regex);
+    var result = match ? match[0] : null; // 如果匹配成功，則取匹配的結果
 
     // 創建超連結元素
     var link = document.createElement('a');
-    link.textContent = ' myGPTs';
+    link.textContent = ' 中央機房';
     if (hostname === "ncueeclass.ncu.edu.tw") {
-        link.href = 'https://nc.uedu.tw/goto/' + content;
+        link.href = 'https://nc.uedu.tw/goto/' + result;
     } else if (hostname === "im.tmu.edu.tw") {
-        link.href = 'https://tm.uedu.tw/goto/' + content;
+        link.href = 'https://tm.uedu.tw/goto/' + result;
     } else {
-        link.href = 'https://uedu.tw/goto/' + content; // 預設連結
+        link.href = 'https://uedu.tw/goto/' + result; // 預設連結
     }
     // link.href = 'https://ncuedu.tw/browser/extension/goto/mygpts/' + content;
 
